b16563ac101bcdb6504137aec85ec4502f81c759
JanithThejaka <janiththejaka2002@gmail.com>
2026-05-11 16:26:27 +0530

    Create Profile module entity

---
+++ fixit_Backend/platform/src/main/java/com/fixit/platform/modules/profile/entity/Profile.java
@@ -0,0 +1,57 @@
+package com.fixit.platform.modules.profile.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "profiles")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private UUID userId;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    private String location;
+
+    private String bio;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "is_provider_profile_complete")
+    private boolean providerProfileComplete = false;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ fixit_Backend/platform/src/main/java/com/fixit/platform/modules/profile/repository/ProfileRepository.java
@@ -0,0 +1,12 @@
+package com.fixit.platform.modules.profile.repository;
+
+import com.fixit.platform.modules.profile.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<Profile,Integer> {
+    Optional<Profile> findByUserId(UUID userId);
+
+}
