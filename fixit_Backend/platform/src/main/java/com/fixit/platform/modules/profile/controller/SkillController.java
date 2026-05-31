c847a6a47e0ba0363fdf05b287e60fcba4b289ec
JanithThejaka <janiththejaka2002@gmail.com>
2026-05-31 19:10:49 +0530

    Add endpoints to skills editing services

--- fixit_Backend/platform/src/main/java/com/fixit/platform/modules/profile/controller/SkillController.java
+++ fixit_Backend/platform/src/main/java/com/fixit/platform/modules/profile/controller/SkillController.java
@@ -1,25 +1,92 @@
 package com.fixit.platform.modules.profile.controller;
 
+import com.fixit.platform.modules.auth.entity.User;
+import com.fixit.platform.modules.auth.repository.UserRepository;
+import com.fixit.platform.modules.profile.dto.AddProviderSkillRequest;
+import com.fixit.platform.modules.profile.dto.ProviderSkillResponse;
 import com.fixit.platform.modules.profile.dto.SkillResponse;
 import com.fixit.platform.modules.profile.service.ProfileService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/api/skills")
 public class SkillController {
 
     private final ProfileService profileService;
+    private final UserRepository userRepository;
 
-    public SkillController(ProfileService profileService) {
+    public SkillController(ProfileService profileService, UserRepository userRepository) {
         this.profileService = profileService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
     public List<SkillResponse> getSkills() {
         return profileService.getAllSkills();
+    }
+
+    // endpoints for add, view , delete skill by provider
+
+    @PostMapping("/provider/skills")
+    @PreAuthorize("hasRole('PROVIDER')")
+    public String addProviderSkill(
+            Authentication authentication,
+            @RequestBody AddProviderSkillRequest request
+    ) {
+
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        profileService.addProviderSkill(
+                user.getId(),
+                request.getSkillId()
+        );
+
+        return "Skill added";
+    }
+
+    @GetMapping("/provider/skills")
+    @PreAuthorize("hasRole('PROVIDER')")
+    public List<ProviderSkillResponse>
+    getProviderSkills(
+            Authentication authentication
+    ) {
+
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        return profileService.getProviderSkills(
+                user.getId()
+        );
+    }
+
+    @DeleteMapping("/provider/skills/{skillId}")
+    @PreAuthorize("hasRole('PROVIDER')")
+    public String removeProviderSkill(
+            Authentication authentication,
+            @PathVariable UUID skillId
+    ) {
+
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        profileService.removeProviderSkill(
+                user.getId(),
+                skillId
+        );
+
+        return "Skill removed";
     }
 }
