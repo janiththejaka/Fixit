a24b87d7830b3efc6e716a32c7ce1f71df8d7abd
JanithThejaka <janiththejaka2002@gmail.com>
2026-06-28 01:13:33 +0530

    feat : Initiate service request module and add enums

---
+++ fixit_Backend/platform/src/main/java/com/fixit/platform/modules/service_request/dao/RequestStatus.java
@@ -0,0 +1,15 @@
+package com.fixit.platform.modules.service_request.dao;
+
+public enum RequestStatus {
+    PENDING,
+
+    ACCEPTED,
+
+    REJECTED,
+
+    IN_PROGRESS,
+
+    COMPLETED,
+
+    CANCELLED
+}
