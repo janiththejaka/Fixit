7418e34b298f058414264d0f6d3bffc4d5c655df
JanithThejaka <janiththejaka2002@gmail.com>
2026-09-15 18:36:28 +0530

    feat : enabling the csrf

--- fixit_Backend/platform/src/main/java/com/fixit/platform/config/SecurityConfig.java
+++ fixit_Backend/platform/src/main/java/com/fixit/platform/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @RequiredArgsConstructor
@@ -25,7 +26,15 @@ public class SecurityConfig {
 
         http
                 .cors(cors -> {})
-                .csrf(csrf -> csrf.disable()) // disable CSRF for APIs
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(
+                                CookieCsrfTokenRepository.withHttpOnlyFalse()
+                        )
+                        .ignoringRequestMatchers(
+                                "/api/auth/login",
+                                "/api/auth/register/**"
+                        )
+                )
                 .authorizeHttpRequests(auth -> auth
                         // Only public auth endpoints (login & register) are permit-all
                         .requestMatchers(
